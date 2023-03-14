760c0169a213672daa3aec2202cdac392b34dbb5
Teerapat Limpawittayakul <tlimpawittayakul@gmail.com>
2023-03-14 14:01:18 +0700

    edit Exercise2.js

---
+++ Exercise2.js
@@ -0,0 +1,12 @@
+// part1
+const greet = (name1,name2,name3) => `Welcome ${name1}, ${name2}, ${name3}.`;
+console.log(greet('a','b','c'));
+
+// part2
+const birth = (year) => 2023-1990;
+console.log(birth(1990));
+
+// part3
+const greetBirth = (name1,name2,name3,age1,age2,age3) => 
+`Welcome ${name1}, you are ${age1}. Welcome ${name2}, you are ${age2}. Welcome ${name3}, you are ${age3}.`
+console.log(greetBirth('m','m','m',14,24,55))
